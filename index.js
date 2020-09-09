158f63e8255becd459f143c4c36e402d72212616
mpizarro <maximilianopi@buenosaires.gob.ar>
2020-09-09 06:19:57 -0700

    test connexion odbc

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 const path = require('path');
 const express = require('express');
+const odbc = require('odbc');
 
 const port = process.env.PORT || 8080;
 const publicRoot = path.resolve(path.join(__dirname, '/'), '');
@@ -13,9 +14,16 @@ app.get('/', (req, res) => {
 const alta = (req, res) => {
     console.log( Date() + ": /alta" );  
     try {
-        return res.json({
-            estado: "success"
-        })
+        const cursor = odbc.connect('DSN=MySQL',(error, cursor)=>{
+          cursor.query('select * from film',
+          (error, result)=>{
+            if(error){
+              return res.send(JSON.stringify(error))
+            }else{
+              return res.send(JSON.stringify(result))
+            }  
+          });
+        });
     } catch (e) {
         console.error( e )
         res.status( 500 )
