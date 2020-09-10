bb4ba77578fbe3552781a1839129e9067a7b4dea
mpizarro <maximilianopi@buenosaires.gob.ar>
2020-09-10 14:49:58 -0700

    test store procedure

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 *Actividad Práctica Estándar ODBC Sistemas Distribuidos 2020 Licenciatura en Sistemas. UNLa.*
 
 
+call nuevocliente({ "store_id": 2,   "last_name": "asda",  "first_name": "dasda",   "email": "dasda@asdas",   "address": "dasda",   "address2": "dasda", "district": "dasda",   "country_id": 1,   "city_id": 251,   "postal_code": "asda",   
+"phone": "dasd",   "active": 1,   location: POINT(-34.7209728,-58.359808) });
+
+
+                #    vlast_name, vfirst_nam),
+                #    vemail,vactive,vaddress,vaddress2,vdistrict,vcity_id,vcountry_id,vpostal_code,vphone,vlocation);
+                                 #    `vlast_name`, `vfirst_name),
+                #    `vemail`,`vactive`,`vaddress`,`vaddress2`,`vdistrict`,`vcity_id`,`vcountry_id`,`vpostal_code`,`vphone`,`vlocation`)
--- nuevocliente.sql
+++ nuevocliente.sql
@@ -3,31 +3,89 @@ DROP PROCEDURE IF EXISTS `nuevocliente`;
 DELIMITER $$
 
 CREATE DEFINER='root'@'localhost' PROCEDURE `nuevocliente` (
-    IN nombre varchar(128), IN apellido varchar(128),
-    IN anio year)
-BEGIN
-    DECLARE done INT DEFAULT FALSE;
-    DECLARE identicador_actor INT;
-    DECLARE identificador_film INT;
-    DECLARE listado CURSOR FOR SELECT film_id FROM film WHERE  film.release_year=anio;
-    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
-
-    INSERT INTO actor (first_name,last_name) VALUES(nombre,apellido);        
-    SELECT max(actor.actor_id) FROM actor INTO identicador_actor;  
-    OPEN listado;
-    read_loop: LOOP
-      FETCH listado INTO identificador_film;
-        IF done THEN
-        LEAVE read_loop;
-        ELSE
-        INSERT INTO film_actor (actor_id,film_id)        
-        VALUES (identicador_actor,identificador_film);
-        END IF;      
-    END LOOP;       
-    CLOSE listado;  
-    SELECT actor.actor_id, actor.first_name, actor.last_name, count(*) FROM actor,film_actor
-    WHERE actor.actor_id=identicador_actor
-    AND actor.actor_id=film_actor.actor_id;  
+IN pParametroJson    JSON)
+BEGIN 
+    DECLARE vJsonEsValido INT;
+    DECLARE vItems INT;
+    DECLARE vIndex BIGINT UNSIGNED DEFAULT 0;
+    
+    # Variables para parseo del objeto JSON
+    DECLARE vstore_id tinyint;
+    DECLARE vlast_name varchar(45);
+    DECLARE vfirst_name varchar(45);
+    DECLARE vemail varchar(50);
+    DECLARE vactive tinyint(1);
+    DECLARE vaddress varchar(50);
+    DECLARE vaddress2 varchar(50);
+    DECLARE vdistrict varchar(20);    
+    DECLARE vcountry_id smallint;
+    DECLARE vcity_id smallint;
+    DECLARE vpostal_code varchar(10);
+    DECLARE vphone varchar(10);
+    DECLARE vlocation geometry;
+    
+    SET vJsonEsValido = JSON_VALID(pParametroJson);
+    
+    IF vJsonEsValido = 0 THEN 
+        # El objeto JSON no es válido, salimos prematuramente
+        SELECT "JSON suministrado no es válido";
+    ELSE 
+        # Nuestro objeto es válido, podemos proceder
+        SET vItems = JSON_LENGTH(pParametroJson);
+        
+        # El objeto es válido y contiene al menos un elemento
+        IF vItems > 0 THEN 
+            # Creamos una tabla temporal donde guardaremos momentáneamente
+            # el contenido del objeto JSON para facilitar su uso
+            DROP TEMPORARY TABLE IF EXISTS `tmp_jsonTest`;
+            CREATE TEMPORARY TABLE `tmp_jsonTest` (
+                `index`        INT(11) NOT NULL,
+                `vstore_id` tinyint,
+                `vlast_name` varchar(45),
+                `vfirst_name` varchar(45),
+                `vemail` varchar(50),
+                `vactive` tinyint(1),
+                `vaddress` varchar(50),
+                `vaddress2` varchar(50),
+                `vdistrict` varchar(20),    
+                `vcountry_id` smallint,
+                `vcity_id` smallint,
+                `vpostal_code` varchar(10),
+                `vphone` varchar(10),
+                `vlocation` geometry
+                        );
+            
+            WHILE vIndex < vItems DO
+                SET vstore_id = JSON_EXTRACT(pParametroJson, 'store_id');
+            #    SET vlast_name = JSON_UNQUOTE(JSON_EXTRACT(pParametroJson, CONCAT('$[', vIndex, '].vlast_name')));                                               
+            #    SET vfirst_name = JSON_UNQUOTE(JSON_EXTRACT(pParametroJson, CONCAT('$[', vIndex, '].first_name')));
+            #    SET vemail = JSON_UNQUOTE(JSON_EXTRACT(pParametroJson, CONCAT('$[', vIndex, '].email')));
+            #    SET vactive = JSON_UNQUOTE(JSON_EXTRACT(pParametroJson, CONCAT('$[', vIndex, '].active')));
+            #    SET vaddress = JSON_UNQUOTE(JSON_EXTRACT(pParametroJson, CONCAT('$[', vIndex, '].address')));
+            #    SET vaddress2 = JSON_UNQUOTE(JSON_EXTRACT(pParametroJson, CONCAT('$[', vIndex, '].address2')));
+            #    SET vdistrict = JSON_UNQUOTE(JSON_EXTRACT(pParametroJson, CONCAT('$[', vIndex, '].district')));    
+            #    SET vcountry_id = JSON_UNQUOTE(JSON_EXTRACT(pParametroJson, CONCAT('$[', vIndex, '].country_id')));
+            #    SET vcity_id = JSON_UNQUOTE(JSON_EXTRACT(pParametroJson, CONCAT('$[', vIndex, '].city_id')));
+            #    SET vpostal_code = JSON_UNQUOTE(JSON_EXTRACT(pParametroJson, CONCAT('$[', vIndex, '].postal_code')));
+            #    SET vphone = JSON_UNQUOTE(JSON_EXTRACT(pParametroJson, CONCAT('$[', vIndex, '].phone')));
+            #    SET vlocation = JSON_EXTRACT(pParametroJson, CONCAT('$[', vIndex, '].location'));
+                INSERT INTO `tmp_jsonTest` 
+                	(`index`,`vstore_id`) 
+                VALUES
+                	(vIndex, vstore_id); 
+   
+                SET vIndex = vIndex + 1;
+            END WHILE;
+            
+            # Aca podemos ya trabajar con el contenido de nuestro JSON
+            # interactuando directamente sobre la tabla temporal utilizando
+            # instrucciones SQL tradicionales
+            
+            # Antes de finalizar es conveniente deshacernos de la
+            # tabla temporal para no dejar basura regada en la base de datos
+            #DROP TEMPORARY TABLE IF EXISTS `tmp_jsonTest`;
+        END IF;
+    END IF;
 END $$
 DELIMITER ;
 
