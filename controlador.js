8ca3fef52def9dbe9a8f9a87ad2ef69e4e3d8777
mpizarro <maximilianopi@buenosaires.gob.ar>
2020-09-12 19:33:36 -0300

    datasource por variable de entorno

--- controlador.js
+++ controlador.js
@@ -1,12 +1,13 @@
 
 const odbc = require('odbc');
+const datasource = process.env.DATASOURCE || 'DSN=MySQL';
 
 module.exports= {
     alta: (req, res) => {
         console.log( Date() + ": /alta" );  
         try {
             console.log(JSON.stringify(req.body, null, 0))
-            const cursor = odbc.connect('DSN=MySQL',(error, cursor)=>{              
+            const cursor = odbc.connect(datasource,(error, cursor)=>{              
               cursor.query('call nuevocliente(?)',[JSON.stringify(req.body, null, 0)],
               (error, result)=>{
                 if(error){
@@ -26,7 +27,7 @@ module.exports= {
         console.log( Date() + ": /modificacion" );  
         try {
             console.log(JSON.stringify(req.body, null, 0))
-            const cursor = odbc.connect('DSN=MySQL',(error, cursor)=>{              
+            const cursor = odbc.connect(datasource,(error, cursor)=>{              
               cursor.query('call modificacliente(?)',[JSON.stringify(req.body, null, 0)],
               (error, result)=>{
                 if(error){
@@ -46,7 +47,7 @@ module.exports= {
         console.log( Date() + ": /baja" );  
         try {
             console.log(JSON.stringify(req.body, null, 0))
-            const cursor = odbc.connect('DSN=MySQL',(error, cursor)=>{              
+            const cursor = odbc.connect(datasource,(error, cursor)=>{              
               cursor.query('call bajacliente(?)',[JSON.stringify(req.body, null, 0)],
               (error, result)=>{
                 if(error){
@@ -65,7 +66,7 @@ module.exports= {
     ciudadesById: (req, res) => {
         console.log( Date() + ": /ciudadesById "+req.body.country_id);  
         try {
-            const cursor = odbc.connect('DSN=MySQL',(error, cursor)=>{
+            const cursor = odbc.connect(datasource,(error, cursor)=>{
               cursor.query('select city_id,city from city where country_id='+req.body.country_id,
               (error, result)=>{
                 if(error){
@@ -85,7 +86,7 @@ module.exports= {
       ciudades: (req, res) => {
         console.log( Date() + ": /ciudades ");  
         try {
-            const cursor = odbc.connect('DSN=MySQL',(error, cursor)=>{
+            const cursor = odbc.connect(datasource,(error, cursor)=>{
               cursor.query('select city_id,city from city',
               (error, result)=>{
                 if(error){
@@ -105,7 +106,7 @@ module.exports= {
     paises: (req, res) => {
         console.log( Date() + ": /paises" );  
         try {
-            const cursor = odbc.connect('DSN=MySQL',(error, cursor)=>{
+            const cursor = odbc.connect(datasource,(error, cursor)=>{
               cursor.query('select country_id,country  from country',
               (error, result)=>{
                 if(error){
@@ -125,7 +126,7 @@ module.exports= {
       tiendas: (req, res) => {
         console.log( Date() + ": /tiendas" );  
         try {
-            const cursor = odbc.connect('DSN=MySQL',(error, cursor)=>{
+            const cursor = odbc.connect(datasource,(error, cursor)=>{
               cursor.query('select store_id, address from store, address limit 2',
               (error, result)=>{
                 if(error){
@@ -145,7 +146,7 @@ module.exports= {
       clientes: (req, res) => {
         console.log( Date() + ": /clientes ");  
         try {
-            const cursor = odbc.connect('DSN=MySQL',(error, cursor)=>{
+            const cursor = odbc.connect(datasource,(error, cursor)=>{
               cursor.query('select customer_id,first_name,last_name from customer order by customer_id desc limit 10',
               (error, result)=>{
                 if(error){
@@ -166,7 +167,7 @@ module.exports= {
         console.log( Date() + ": /buscar ");  
         try {
             console.log(JSON.stringify(req.body, null, 0))
-            const cursor = odbc.connect('DSN=MySQL',(error, cursor)=>{
+            const cursor = odbc.connect(datasource,(error, cursor)=>{
               cursor.query('call busquedanombreciudad(?)',[JSON.stringify(req.body, null, 0)],
               (error, result)=>{
                 if(error){
@@ -186,7 +187,7 @@ module.exports= {
       clientesById: (req, res) => {
         console.log( Date() + ": /clientesById "+req.body.customer_id);  
         try {
-            const cursor = odbc.connect('DSN=MySQL',(error, cursor)=>{
+            const cursor = odbc.connect(datasource,(error, cursor)=>{
               cursor.query('call busquedaid('+req.body.customer_id+')',
               (error, result)=>{
                 if(error){
